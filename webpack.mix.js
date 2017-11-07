3a4514936a8879e178a04ebd57493719996230ef
Hasan Su'udi <keotik@gmail.com>
2017-11-07 23:42:25 +0700

    - install semantic ui - make default auth

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,10 @@ let mix = require('laravel-mix');
  |
  */
 
+// mix.js('resources/assets/js/app.js', 'public/js')
+//    .sass('resources/assets/sass/app.scss', 'public/css');
+
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+   .sass('resources/assets/sass/app.scss', 'public/css')
+   .copy('node_modules/semantic-ui-css/semantic.min.css','public/css/semantic.min.css')
+   .copy('node_modules/semantic-ui-css/semantic.min.js','public/js/semantic.min.js');
