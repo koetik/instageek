6a211e143e3b2ea27299ef46fa5bb0a39c9696bb
Hasan Su'udi <keotik@gmail.com>
2017-11-09 00:37:49 +0700

    create model & migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,18 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('name');
+            $table->text('bio')->nullable();
+            $table->string('website')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('gender', ['M', 'F']);
+            $table->string('profile_picture')->nullable();
+            $table->integer('post_count')->default(0);
+            $table->integer('follower_count')->default(0);
+            $table->integer('following_count')->default(0);
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2017_11_08_171602_create_trigger_post_insert.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTriggerPostInsert extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         DB::unprepared('
+            CREATE TRIGGER post_insert AFTER INSERT ON posts FOR EACH ROW
+            BEGIN
+                UPDATE users SET post_count = post_count+1
+                WHERE id = NEW.user_id;
+            END
+        ');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        DB::unprepared('DROP TRIGGER `post_insert`');
+    }
+}
